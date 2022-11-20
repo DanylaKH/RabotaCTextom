987a9294850bea162725ffdfcc7ae2d0ed5ad311
DanylaKH <hristyushin.danil@gmail.com>
2022-11-20 06:43:50 +0200

    DataBaseRetriever - uppdate

--- RabotaCTextom/Config.cs
+++ RabotaCTextom/Config.cs
@@ -8,7 +8,7 @@ namespace RabotaCTextom
     class Config
     {
         public string path = "C:\\Users\\admin\\Documents\\test.txt";
-        private string dbConnect = null;
-
+        public string dbConnect = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\admin\\Desktop\\RabotaCTextom\\RabotaCTextom\\DatabaseTest.mdf;Integrated Security=True";
+        public string queryString = "SELECT Items FROM TestTable";
     }
 }
--- RabotaCTextom/DatabaseDataRetriever.cs
+++ RabotaCTextom/DatabaseDataRetriever.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace RabotaCTextom
 {
@@ -8,10 +10,20 @@ namespace RabotaCTextom
         {
             try
             {
-                Console.WriteLine("Read from database");
-                string bdlines = Console.ReadLine();
-                Console.WriteLine("Read from database - success");
-                return bdlines;
+                string outputstring = null;
+                List<string> word = new List<string>();
+                Config cfg = new Config();
+                SqlConnection connectionString = new SqlConnection(cfg.dbConnect);
+                connectionString.Open();
+                Console.WriteLine("Database connection open");
+                SqlCommand comand = new SqlCommand(cfg.queryString, connectionString);
+                SqlDataReader dataReader = comand.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    word.Add(dataReader.GetString(0));
+                }
+                dataReader.Close();
+                return outputstring = string.Join(" ", word);
             }
             catch
             {
